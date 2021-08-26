6e6f93573a4e65235a66baaade9293f9c52fed07
komson <shootingstar7447@gmail.com>
2021-08-26 16:50:39 +0700

    add seperate error for invalid params

--- app/controllers/ex2_controller.rb
+++ app/controllers/ex2_controller.rb
@@ -2,14 +2,21 @@ class Ex2Controller < ApplicationController
   def loop
     a = params[:a]
     b = params[:b]
-    if Integer(a, exception: false) && Integer(b, exception: false)
-      puts a.class
-      puts b.class
-      @results = (a..b).to_a
+    @results = Array.new
+    isAInt = false
+    isBInt = false
+    if Integer(a, exception: false)
+      isAInt = true
     else
-      puts a.class
-      puts b.class
-      @results = ['Invalid parameter a or b']
+      @results.concat(['Invalid parameter for a'])
+    end
+    if Integer(b, exception: false)
+      isBInt = true
+    else
+      @results.concat(['Invalid parameter for b'])
+    end
+    if isAInt && isBInt
+      @results = (a..b).to_a
     end
   end
 end
