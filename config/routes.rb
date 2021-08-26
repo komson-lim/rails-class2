13c4815db1f209f20aba25b71d94064d3a21cc75
komson <shootingstar7447@gmail.com>
2021-08-26 15:00:31 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'main/abc'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome', to: 'main#welcome'
+  get 'contact_us', to: 'main#contact'
+end
