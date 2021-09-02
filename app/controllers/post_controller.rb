0045b2ca90509d5ea6b41ec0bee8387b253bb881
komson <shootingstar7447@gmail.com>
2021-09-02 20:41:36 +0700

    hw: class3 add exception

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,7 +2,7 @@ class PostController < ApplicationController
   def create
     @msg = params[:msg]
     @user_id = params[:user_id]
-    Post.create(msg: @msg, user_id: @user_id)
+    @post = Post.create(msg: @msg, user_id: @user_id)
     render 'create2'
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,13 @@
 class UserController < ApplicationController
   def read
+    @valid_id = true
     @user_id = params[:user_id]
     @user = User.find(@user_id)
+  rescue ActiveRecord::RecordNotFound
+    @valid_id = false
+  else
     @posts = @user.posts
+  ensure
     render 'read2'
   end
 end
